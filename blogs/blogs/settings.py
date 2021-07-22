5cbf31645839a9c2aea53ec0fafc4e4d482ac30c
Shandriuk <shandrug.nikita@gmail.com>
2021-07-22 06:36:44 -0700

    modified settings.py

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2oicpqol7@8(!-05ks(td0@q@!#qk@)lhhj-#*3a=%5ngxho1g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
